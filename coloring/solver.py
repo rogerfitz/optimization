e114a9aa0e46c5cbc2eed2cdebce3c1817eb0604
rogerfitz <minime@hochweb.com>
2015-04-17 15:28:32 -0500

    Added greedy one pass algorithm

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,10 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from collections import defaultdict
+import numpy as np
+from sortedcontainers import SortedSet
+
+def nodeInvertedIndex(edges):
+	idx = defaultdict(SortedSet)#can change to numpy array
+	for i in range(len(edges)):
+		idx[edges[i][0]].add(edges[i][1])
+	return idx
 
 def basicGreedy(edges, node_count):
     # build a trivial solution
     # every node has its own color
     return range(0, node_count)
+
+def improvedGreedy(edges, node_count):
+	# returns onePass greedy colors algorithm
+	
+	ndx = nodeInvertedIndex(edges)#actually node edge index
+	colors = np.arange((node_count), dtype='uint16')
+	return onePass(ndx, colors)
+
+def onePass(ndx, colors):
+	# Outputs feasible space algorithm.
+	# 1 pass check neighbors and assign lowest color
+	for i in range(len(colors)):
+		colors[i]=0
+		j=0
+		while j < len(ndx[i]):
+			if colors[i] == colors[ndx[i][j]]:
+				colors[i] += 1
+				j=-1
+			j+=1
+	return colors
+
+def localKempe(edges, node_count):
+
+	pass
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -21,11 +54,13 @@ def solve_it(input_data):
         line = lines[i]
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
-
-	solution = basicGreedy(edges, node_count)
+        edges.append((int(parts[1]), int(parts[0])))
+	
+    solution = improvedGreedy(edges, node_count)
+	#solution = basicGreedy(edges, node_count)
 
     # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
+    output_data = str(max(solution)+1) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, solution))
 
     return output_data
